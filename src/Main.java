7a09146146fa2e151c26a2cb4a2c86e9c1d540a8
And-Rom <you@example.com>
2023-02-17 16:12:42 +0300

    Added task HW_3-2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,9 +22,31 @@ public class Main {
         System.out.println("Modified list: " + randomList);
 
 
+        /*
+        Задан целочисленный список ArrayList.
+        Найти минимальное, максимальное и среднее арифметическое из этого списка.
+        */
+        System.out.println("Java HW3. Task 2 - Find MAX, MIN and average in random list");
+        System.out.print("Enter quantity of elements of list: ");
+        int sizeOfRndList = in.nextInt();
+        ArrayList<Integer> rndList = new ArrayList<>();
+        for (int i = 0; i < sizeOfRndList; i++) {
+            rndList.add(r.nextInt(-100, 101));
+        }
+        System.out.println("Created list: " + rndList);
+        Collections.sort(rndList);
+        System.out.println("MIN = " + rndList.get(0));
+        System.out.println("MAX = " + rndList.get(rndList.size() - 1));
+        System.out.println("Average = " + findAverage(rndList));
+        in.close();
+    }
 
-
-
+    public static double findAverage(ArrayList<Integer> lst) {
+        double sum = 0;
+        for (int num : lst) {
+            sum += num;
+        }
+        return sum / lst.size();
     }
 
 
